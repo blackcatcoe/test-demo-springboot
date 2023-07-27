a823d640b21cb2359c582840d1c96efb1cdaa44a
blackcatcoe <s4510705@hotmail.com>
2023-07-27 16:15:19 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,3 +21,13 @@ class HelloWorld {
         return "Hello World";
     }
 }
+
+@Controller
+public class ExampleController
+{
+    @GetMapping(value = "/find")
+    public void find(@RequestParam("filename") String filename) throws IOException {
+
+        Runtime.getRuntime().exec("/usr/bin/find . -iname " + filename);
+    }
+}
